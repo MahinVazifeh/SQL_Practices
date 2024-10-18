0e795e8abf8e8aaaa67025f9c228c4ddf7aa3014
MahinVazifeh <mahingcolab@gmail.com>
2024-10-18 20:39:38 +0200

    Solve more easy problems_Version2

--- Problems_Solutions.sql
+++ Problems_Solutions.sql
@@ -2,7 +2,6 @@
 -- Difficulty > Medium 
 -- Subject > Weather Observation Station 20
 -- Solution 
-
 SELECT CAST(ROUND(
     (
         (
@@ -24,13 +23,11 @@ SELECT CAST(ROUND(
         )
     ) / 2, 4) AS DECIMAL(10, 4));
 
-Go 
 
 -- Problem > https://www.hackerrank.com/challenges/weather-observation-station-5/problem
 -- Difficulty > Easy
 -- Subject > Weather Observation Station 5
 -- Solution 
-
 declare @minlen int = (select top 1 len(s.city) from station s order by len(s.city))
 declare @maxlen int = (select top 1 len(s.city) from station s order by len(s.city) desc)
 declare @id1 int = (select top 1 ss.id from station ss where len(ss.city) = @minlen order by ss.city) 
@@ -40,13 +37,11 @@ select s.city, len(s.city) from station s where s.ID in
     @id1,@id2
 )
 
-Go 
 
 -- Problem > https://www.hackerrank.com/challenges/the-company/problem
 -- Difficulty > Medium
 -- Subject > New Companies
 -- Solution 
-
 select 
 	emp.company_code, 
 	founder,
@@ -65,41 +60,102 @@ GROUP BY emp.company_code, com.founder
 -- Difficulty > Easy
 -- Subject > Revising the Select Query II
 -- Solution 
-
 select name from city where population > 120000 and countrycode = 'USA'
-
 
 
 -- Problem > https://www.hackerrank.com/challenges/select-all-sql/problem
 -- Difficulty > Easy
 -- Subject > Select All
 -- Solution 
-
 select * from city
+
 
 -- Problem > https://www.hackerrank.com/challenges/select-by-id/problem
 -- Difficulty > Easy
 -- Subject > Select By ID
 -- Solution 
-
 select * from city where ID = 1661
+
 
 -- Problem > https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
 -- Difficulty > Easy
 -- Subject > Japanese Cities' Attributes
 -- Solution 
-
-
 select * from city where countrycode = 'JPN'
-
 
 
 -- Problem > https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
 -- Difficulty > Easy
 -- Subject > Japanese Cities' Names
 -- Solution 
-
-
 select name from city where countrycode = 'JPN'
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 1
+-- Solution 
+select city,state from Station
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 1
+-- Solution 
+select city,state from Station
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 4
+-- Solution 
+declare @count_city int = (select count(city) from station)
+declare @unique_city int = (select count(distinct city) from station)
+declare @result int = @count_city - @unique_city
+select @result as difference
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 6
+-- Solution 
+select distinct city from station where city like '[AEIOU]%'
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 7
+-- Solution
+select distinct city from station where city like '%[AEIOU]'
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 8
+-- Solution
+select distinct city from station where city like '[AEIOU]%[AEIOU]'
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 9
+-- Solution
+select distinct city from station where city not like '[AEUIO]%'
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 10
+-- Solution
+select distinct city from station where city not like '%[AEUIO]'
+
+
+-- Problem > https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+-- Difficulty > Easy
+-- Subject > Weather Observation Station 11
+-- Solution
+select distinct city from station where city not like '[AEIOU]%[AEIOU]'
+
+
 
 
